cf8acc0ad9f5ab0c80bc920255e0db279ce8ded8
gerard ziemski <63425797+gerard-ziemski@users.noreply.github.com>
2025-01-18 08:21:18 -0600

    move the optional tool into it's own issue/PR

--- src/utils/nmt/Statistical.java
+++
@@ -1,8 +0,0 @@
-package nmt;
-
-public interface Statistical
-  {
-    public boolean addStatistics(NMT_Allocation ai);
-    public void clearStatistics();
-    public MemoryStats getStatistics();
-  }
